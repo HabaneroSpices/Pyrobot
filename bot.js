65c110cd7b324de051cc6b9a1d2186bf3c9d6fd5
Habanerospices <habanerospices@gmail.com>
2022-01-30 19:17:44 +0100

    End of day

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-
 const Discord = require("discord.js");
+
+const generateImage = require("./generateImage.js");
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 });
 
@@ -19,6 +21,14 @@ client.on("messageCreate", message => {
             message.reply("\u{1F3D3} Pong!");
         }
     }
+});
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    });
 });
 
 client.login(process.env.TOKEN); // Authenticate bot with API token in ./.env
